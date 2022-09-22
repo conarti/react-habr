b0cb077d5a1afdd5fe9cb29898d230276aad0435
conarti <abelous2009@gmail.com>
2022-09-22 18:42:23 +0300

    chore(webpack config): update default devServer port

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
+import path from 'path';
 import webpack from 'webpack';
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 import { BuildEnv, BuildPaths } from './config/build/types/config';
-import path from 'path';
 
 export default (env: BuildEnv) => {
     const paths: BuildPaths = {
@@ -13,7 +13,7 @@ export default (env: BuildEnv) => {
 
     const mode = env.mode ?? 'development';
     const isDev = mode === 'development';
-    const PORT = env.port ?? 3000;
+    const PORT = env.port ?? 8080;
 
     const config: webpack.Configuration = buildWebpackConfig({
         mode,
