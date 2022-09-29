8e3f9ebb0297506dc48f0c9d43116353dadd8d44
Aleksandr Belous <abelous2009@gmail.com>
2022-09-29 19:39:51 +0300

    feat(ThemeSwitcher): replace default btn with AppButton

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,6 +2,7 @@ import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { AppButton, ThemeButton } from 'shared/ui/AppButton';
 import cls from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
@@ -12,8 +13,12 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     const { theme, toggleTheme } = useTheme();
 
     return (
-        <button className={classNames(cls.themeSwitcher, {}, [className])} onClick={toggleTheme}>
+        <AppButton
+            className={classNames(cls.themeSwitcher, {}, [className])}
+            theme={ThemeButton.CLEAR}
+            onClick={toggleTheme}
+        >
             {theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
-        </button>
+        </AppButton>
     );
 };
