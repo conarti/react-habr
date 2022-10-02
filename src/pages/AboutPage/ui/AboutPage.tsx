d71eb9a33ced500a1c004408b775b496ab136aa9
Aleksandr Belous <abelous2009@gmail.com>
2022-10-02 16:13:54 +0300

    fix: add missed translations

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-const AboutPage = () => (
-	<div>
-		About page
-	</div>
-);
+const AboutPage = () => {
+	const { t } = useTranslation('about');
+
+	return (
+		<div>
+			{t('О сайте')}
+		</div>
+	);
+};
 
 export default AboutPage;
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
-const MainPage = () => (
-	<div>
-		Main Page
-	</div>
-);
+const MainPage = () => {
+	const { t } = useTranslation('main');
+	return (
+		<div>
+			{t('Главная')}
+		</div>
+	);
+};
 
 export default MainPage;
