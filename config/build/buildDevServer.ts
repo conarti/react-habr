468d61490f4465f17fd4b7a0458da1ea11cfed1e
Aleksandr Belous <abelous2009@gmail.com>
2022-10-02 14:21:56 +0300

    chore: add HMR plugins

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -1,9 +1,10 @@
-import { BuildOptions } from './types/config';
 import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
+import { BuildOptions } from './types/config';
 
 export const buildDevServer = (options: BuildOptions): DevServerConfiguration => {
     return {
         port: options.port,
         historyApiFallback: true,
+        hot: true,
     };
 };
