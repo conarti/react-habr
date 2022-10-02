9ee60e75304f718c43009ac29cad543ae9a3f7d9
Aleksandr Belous <abelous2009@gmail.com>
2022-10-02 22:32:03 +0300

    chore: add bundle analyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export const buildPlugins = ({ paths, isDev }: BuildOptions) => {
@@ -16,6 +17,9 @@ export const buildPlugins = ({ paths, isDev }: BuildOptions) => {
 		}),
 		new webpack.DefinePlugin({
 			__IS_DEV__: JSON.stringify(isDev),
+		}),
+		new BundleAnalyzerPlugin({
+			openAnalyzer: false,
 		}),
 	];
 
