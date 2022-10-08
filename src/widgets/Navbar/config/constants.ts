0ddb3faea11ae81e0e7b318fc5d4b3c419362f45
Aleksandr Belous <abelous2009@gmail.com>
2022-10-08 21:56:20 +0300

    refactor(Navbar): move 'links' to constants

---
+++ src/widgets/Navbar/config/constants.ts
@@ -0,0 +1,10 @@
+export const links = [
+	{
+		to: '/',
+		label: 'Main',
+	},
+	{
+		to: '/about',
+		label: 'About',
+	},
+];
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,38 +1,26 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink';
+import { links } from '../config';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => {
-	const links = [
-		{
-			to: '/',
-			label: 'Main',
-		},
-		{
-			to: '/about',
-			label: 'About',
-		},
-	];
-
-	return (
-		<div className={classNames(cls.navbar, {}, [className])}>
-			<div className={classNames(cls.links)}>
-				{
-					links.map(({ to, label }) => (
-						<AppLink
-							theme={AppLinkTheme.SECONDARY}
-							to={to}
-							key={to}
-						>
-							{label}
-						</AppLink>
-					))
-				}
-			</div>
+export const Navbar = ({ className }: NavbarProps) => (
+	<div className={classNames(cls.navbar, {}, [className])}>
+		<div className={classNames(cls.links)}>
+			{
+				links.map(({ to, label }) => (
+					<AppLink
+						theme={AppLinkTheme.SECONDARY}
+						to={to}
+						key={to}
+					>
+						{label}
+					</AppLink>
+				))
+			}
 		</div>
-	);
-};
+	</div>
+);
