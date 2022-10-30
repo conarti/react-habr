a7655f9a8430524c05345dc7b439b071916de4ab
Aleksandr Belous <abelous2009@gmail.com>
2022-10-30 20:07:15 +0300

    feat(shared ui): setup AppInput component

---
+++ src/shared/ui/AppInput/AppInput.tsx
@@ -0,0 +1,47 @@
+import classNames from 'classnames';
+import { ChangeEvent, InputHTMLAttributes } from 'react';
+import { uniqueId } from 'shared/lib/uniqueId/uniqueId';
+import cls from './AppInput.module.scss';
+
+interface AppInputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onInput'>{
+    className?: string;
+		label?: string;
+		isFill?: boolean;
+		value: string;
+		onInput: (value: string) => void;
+}
+
+export const AppInput = (props: AppInputProps) => {
+	const {
+		className,
+		label,
+		value,
+		onInput,
+		id = uniqueId(),
+		isFill = false,
+		type = 'text',
+		...otherProps
+	} = props;
+
+	const onInputHandler = (ev: ChangeEvent<HTMLInputElement>) => {
+		onInput(ev.target.value);
+	};
+
+	return (
+		<div className={classNames(cls.appInput, className, { [cls.appInputIsFill]: isFill })}>
+			{label && (
+				<label htmlFor={id}>
+					{label}
+				</label>
+			)}
+			<input
+				id={id}
+				className={classNames(cls.appInputNative)}
+				type={type}
+				value={value}
+				onInput={onInputHandler}
+				{...otherProps}
+			/>
+		</div>
+	);
+};
