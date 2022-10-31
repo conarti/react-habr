cca0b2c02da47b59c49aeeef77a4b18c8da395e7
Aleksandr Belous <abelous2009@gmail.com>
2022-10-31 12:52:24 +0300

    chore(AppInput): setup stories

---
+++ src/shared/ui/AppInput/AppInput.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { AppInput } from './AppInput';
+
+export default {
+	title: 'shared/AppInput',
+	component: AppInput,
+	args: {
+		type: 'text',
+	},
+} as ComponentMeta<typeof AppInput>;
+
+const Template: ComponentStory<typeof AppInput> = (args) => <AppInput {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const WithLabel = Template.bind({});
+WithLabel.args = {
+	label: 'Input label',
+};
