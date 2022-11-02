687e1b292b4af2cef537c0b66009b7260ba5589f
conarti <abelous2009@gmail.com>
2022-11-02 16:26:17 +0300

    feat(LocalStorage helper): add 'remove' method

--- src/shared/lib/LocalStorage.ts
+++ src/shared/lib/LocalStorage.ts
@@ -6,4 +6,8 @@ export class LocalStorage {
 	public static get(key: string, defaultValue?: any) {
 		return JSON.parse(localStorage.getItem(key)) ?? defaultValue;
 	}
+
+	public static remove(key: string) {
+		localStorage.removeItem(key);
+	}
 }
