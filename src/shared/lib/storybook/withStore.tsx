63bfa0ff0393733b220a878a6046d99f68284892
conarti <abelous2009@gmail.com>
2022-11-03 18:06:55 +0300

    chore(storybook): setup async reducers for stories

--- src/shared/lib/storybook/withStore.tsx
+++ src/shared/lib/storybook/withStore.tsx
@@ -1,9 +1,12 @@
-import { DeepPartial } from '@reduxjs/toolkit';
+import { DeepPartial, ReducersMapObject } from '@reduxjs/toolkit';
 import { Story } from '@storybook/react';
 import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 
-export const withStore = (state: DeepPartial<StateSchema>) => (StoryComponent: Story) => (
-	<StoreProvider initialState={state}>
+export const withStore = (state: DeepPartial<StateSchema>, asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>) => (StoryComponent: Story) => (
+	<StoreProvider
+		initialState={state}
+		asyncReducers={asyncReducers}
+	>
 		<StoryComponent />
 	</StoreProvider>
 );
