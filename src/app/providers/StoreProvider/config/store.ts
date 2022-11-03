4783b1dc93a962f9d3a476b45bee153ea827a2ac
conarti <abelous2009@gmail.com>
2022-11-03 16:03:46 +0300

    feat(global store): setup reducerManager for manage async reducers

--- src/app/providers/StoreProvider/config/store.ts
+++ src/app/providers/StoreProvider/config/store.ts
@@ -1,19 +1,26 @@
 import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
+import { createReducerManager } from 'app/providers/StoreProvider/config/reducerManager';
 import { counterReducer } from 'entities/Counter';
 import { userModel } from 'entities/user';
-import { loginReducer } from 'features/auth-by-username';
 import { StateSchema } from './types';
 
-export const createReduxStore = (initialState?: StateSchema) => {
+export const createReduxStore = (initialState?: StateSchema, asyncReducers?: ReducersMapObject<StateSchema>) => {
 	const rootReducer: ReducersMapObject<StateSchema> = {
 		counter: counterReducer,
 		user: userModel.userReducer,
-		loginForm: loginReducer,
+		...asyncReducers,
 	};
 
-	return configureStore<StateSchema>({
-		reducer: rootReducer,
+	const reducerManager = createReducerManager(rootReducer);
+
+	const store = configureStore<StateSchema>({
+		reducer: reducerManager.reduce,
 		devTools: __IS_DEV__,
 		preloadedState: initialState,
 	});
+
+	// @ts-ignore
+	store.reducerManager = reducerManager;
+
+	return store;
 };
--- src/app/providers/StoreProvider/config/types.ts
+++ src/app/providers/StoreProvider/config/types.ts
@@ -1,9 +1,26 @@
+import {
+	AnyAction, EnhancedStore, Reducer, ReducersMapObject,
+} from '@reduxjs/toolkit';
 import { CounterSchema } from 'entities/Counter';
 import { UserSchema } from 'entities/user/config';
 import { LoginSchema } from 'features/auth-by-username';
+import { CombinedState } from 'redux';
 
 export interface StateSchema {
 	counter: CounterSchema;
 	user: UserSchema;
-	loginForm: LoginSchema;
+	loginForm?: LoginSchema;
+}
+
+export type StateSchemaKey = keyof StateSchema;
+
+export interface ReducerManager {
+	getReducerMap: () => ReducersMapObject<StateSchema>;
+	reduce: (state: StateSchema, action: AnyAction) => CombinedState<StateSchema>;
+	add: (key: StateSchemaKey, reducer: Reducer) => void;
+	remove: (key: StateSchemaKey) => void;
+}
+
+export interface ReduxStoreWithManager extends EnhancedStore<StateSchema> {
+	reducerManager: ReducerManager;
 }
