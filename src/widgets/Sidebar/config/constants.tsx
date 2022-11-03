e30e55e0898623fb1f18e451e258e2fcfe1106fb
conarti <abelous2009@gmail.com>
2022-11-03 20:02:38 +0300

    refactor(sidebar/links): replace 'to' paths to AppRoutes constants

--- src/widgets/Sidebar/config/constants.tsx
+++ src/widgets/Sidebar/config/constants.tsx
@@ -1,14 +1,15 @@
+import { AppRoutes } from 'pages';
 import InformationIcon from 'shared/assets/icons/circle-information.svg';
 import HouseIcon from 'shared/assets/icons/house-floor.svg';
 
 export const links = [
 	{
-		to: '/',
+		to: AppRoutes.MAIN,
 		label: 'Главная',
 		icon: <HouseIcon />,
 	},
 	{
-		to: '/about',
+		to: AppRoutes.ABOUT,
 		label: 'О сайте',
 		icon: <InformationIcon />,
 	},
