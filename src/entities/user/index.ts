48b9e8bda7b123354bc6d210db5cd8d12effc284
conarti <abelous2009@gmail.com>
2022-11-03 21:33:52 +0300

    feat(entities/user): setup ProfileMenu

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,2 +1,3 @@
 export * as userModel from './model';
 export * as userConfig from './config';
+export { ProfileMenu } from './ui/ProfileMenu';
---
+++ src/entities/user/ui/ProfileMenu/ProfileMenu.tsx
@@ -0,0 +1,18 @@
+import classNames from 'classnames';
+import { t } from 'i18next';
+import { AppRoutes } from 'pages';
+import { AppLink } from 'shared/ui/AppLink';
+import styles from './ProfileMenu.module.scss';
+
+interface ProfileMenuProps {
+    className?: string;
+}
+
+export const ProfileMenu = ({ className }: ProfileMenuProps) => (
+	<div className={classNames(styles.profileMenu, className)}>
+		{/* eslint-disable-next-line i18next/no-literal-string */}
+		<AppLink to={AppRoutes.PROFILE}>
+			Профиль
+		</AppLink>
+	</div>
+);
