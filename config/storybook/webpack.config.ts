f24e93affb53d0f30969fee27afd6ca554909e65
conarti <abelous2009@gmail.com>
2022-11-15 00:46:51 +0300

    refactor(storybook webpack config): fix ts 'strict: true' errors

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -4,25 +4,26 @@ import { buildSassLoader } from '../build/loaders/buildSassLoader';
 import { buildSvgLoader } from '../build/loaders/buildSvgLoader';
 import { BuildPaths } from '../build/types/config';
 
-export default ({ config }: {config: webpack.Configuration}) => {
+export default ({ config }: { config: webpack.Configuration }) => {
 	const paths: BuildPaths = {
 		build: '',
 		html: '',
 		entry: '',
 		src: path.resolve(__dirname, '..', '..', 'src'),
 	};
-	config.resolve.modules.push(paths.src);
-	config.resolve.extensions.push('.ts', '.tsx');
+	config!.resolve!.modules!.push(paths.src);
+	config!.resolve!.extensions!.push('.ts', '.tsx');
 	// eslint-disable-next-line no-param-reassign
-	config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
+	// @ts-ignore
+	config!.module!.rules = config!.module!.rules!.map((rule: RuleSetRule) => {
 		if (/svg/.test(rule.test as string)) {
 			return { ...rule, exclude: /\.svg$/i };
 		}
 
 		return rule;
 	});
-	config.module.rules.push(buildSassLoader(true), buildSvgLoader());
-	config.plugins.push(new DefinePlugin({
+	config!.module!.rules.push(buildSassLoader(true), buildSvgLoader());
+	config!.plugins!.push(new DefinePlugin({
 		__IS_DEV__: true,
 	}));
 	return config;
