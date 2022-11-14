bc73500e11d6dbb913671b3967bca288f8124a79
conarti <abelous2009@gmail.com>
2022-11-14 23:04:39 +0300

    refactor(api): return 'undefined' if ls has not user data

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -5,6 +5,6 @@ import { LocalStorage } from 'shared/lib/LocalStorage/LocalStorage';
 export const $api = axios.create({
 	baseURL: process.env.API_URL,
 	headers: {
-		authorization: LocalStorage.get(userConfig.USER_LOCAL_STORAGE_KEY),
+		authorization: LocalStorage.get(userConfig.USER_LOCAL_STORAGE_KEY, undefined),
 	},
 });
