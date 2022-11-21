81d7855ec2922c45c64cad3f61016e46428cae2f
conarti <abelous2009@gmail.com>
2022-11-21 18:43:59 +0300

    feat(pages): setup Articles page

---
+++ src/pages/ArticlesPage/ArticlesPage.tsx
@@ -0,0 +1,10 @@
+import { memo } from 'react';
+
+const ArticlesPage = memo(() => (
+	// eslint-disable-next-line i18next/no-literal-string
+	<div>
+		Articles Page
+	</div>
+));
+
+export default ArticlesPage;
