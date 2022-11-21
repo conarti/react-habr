2a93fdfb29d64508dd3003f7fbb71983977fa0ae
conarti <abelous2009@gmail.com>
2022-11-21 11:59:28 +0300

    chore(shared/api): add todo

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { userConfig } from 'entities/user';
 import { LocalStorage } from 'shared/lib/LocalStorage/LocalStorage';
 
+// TODO: normal authorization
 export const $api = axios.create({
 	baseURL: process.env.API_URL,
 	headers: {
