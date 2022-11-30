c0df025830c83ecd108e7ab154cfd63f383664b3
conarti <abelous2009@gmail.com>
2022-11-30 17:33:59 +0300

    feat(CommentCard): open user profile after clicking on the profile picture

--- src/entities/comment/ui/CommentCard/CommentCard.tsx
+++ src/entities/comment/ui/CommentCard/CommentCard.tsx
@@ -1,7 +1,9 @@
 import classNames from 'classnames';
+import { AppRoutes } from 'pages';
 import { memo } from 'react';
 import { AppAvatar } from 'shared/ui/AppAvatar';
 import { AppCard } from 'shared/ui/AppCard';
+import { AppLink } from 'shared/ui/AppLink';
 import { Comment } from '../../config';
 import styles from './CommentCard.module.scss';
 
@@ -19,13 +21,16 @@ export const CommentCard = memo((props: CommentCardProps) => {
 	return (
 		<AppCard className={classNames(styles.commentCard, className)}>
 			<>
-				<div className={classNames(styles.commentCardHeader)}>
+				<AppLink
+					className={classNames(styles.commentCardHeader)}
+					to={`${AppRoutes.PROFILE}/${comment.user.id}`}
+				>
 					<AppAvatar
 						size="sm"
 						src={comment.user.avatar}
 					/>
 					<span className={classNames(styles.commentCardUsername)}>{comment.user.username}</span>
-				</div>
+				</AppLink>
 				<p>{comment.message}</p>
 			</>
 		</AppCard>
