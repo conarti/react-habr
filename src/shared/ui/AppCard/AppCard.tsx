a6aa408c9eb06cd308a3d6b492bd017b61ef2e55
conarti <abelous2009@gmail.com>
2022-12-01 13:56:46 +0300

    feat(AppCard): add 'cover' image

--- src/shared/ui/AppCard/AppCard.tsx
+++ src/shared/ui/AppCard/AppCard.tsx
@@ -5,10 +5,28 @@ import cls from './AppCard.module.scss';
 interface AppCardProps {
     className?: string;
 		children: ReactNode;
+		cover?: string;
 }
 
-export const AppCard = ({ className, children }: AppCardProps) => (
-	<div className={classNames(cls.appCard, className)}>
-		{children}
-	</div>
-);
+export const AppCard = (props: AppCardProps) => {
+	const {
+		className,
+		children,
+		cover,
+	} = props;
+
+	return (
+		<div className={classNames(cls.appCard, className)}>
+			{cover && (
+				<img
+					className={classNames(cls.appCardCover)}
+					src={cover}
+					alt="card cover"
+				/>
+			)}
+			<div className={classNames(cls.appCardBody)}>
+				{children}
+			</div>
+		</div>
+	);
+};
