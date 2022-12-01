5fc366d4be95407c2b4b65c8ea8d44190db7082a
conarti <abelous2009@gmail.com>
2022-12-01 18:03:07 +0300

    feat(ArticleInfoRow): add 'justify' prop

--- src/entities/article/ui/ArticleInfo/ArticleInfoRow/ArticleInfoRow.tsx
+++ src/entities/article/ui/ArticleInfo/ArticleInfoRow/ArticleInfoRow.tsx
@@ -5,16 +5,28 @@ import styles from './ArticleInfoRow.module.scss';
 interface ArticleInfoRowProps {
 	className?: string;
   children: ReactNode;
+	justify?: 'start' | 'end' | 'center' | 'between'
 }
 
 export const ArticleInfoRow = (props: ArticleInfoRowProps) => {
 	const {
 		className,
 		children,
+		justify = 'start',
 	} = props;
 
 	return (
-		<div className={classNames(styles.articleInfoRow, className)}>
+		<div className={classNames(
+			styles.articleInfoRow,
+			className,
+			{
+				[styles.isJustifyStart]: justify === 'start',
+				[styles.isJustifyCenter]: justify === 'center',
+				[styles.isJustifyEnd]: justify === 'end',
+				[styles.isJustifyBetween]: justify === 'between',
+			},
+		)}
+		>
 			{children}
 		</div>
 	);
