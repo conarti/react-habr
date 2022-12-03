b2d057fbd654dcea83a48b45d525b3e1332130ee
conarti <abelous2009@gmail.com>
2022-12-03 09:55:55 +0300

    feat(AppInput): setup skeleton

---
+++ src/shared/ui/AppInput/AppInput.skeleton.tsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames';
+import { AppSkeleton } from 'shared/ui/AppSkeleton';
+import styles from './AppInput.module.scss';
+
+interface AppInputSkeletonProps {
+	className?: string;
+	hasLabel?: boolean;
+}
+
+export const AppInputSkeleton = (props: AppInputSkeletonProps) => {
+	const {
+		className,
+		hasLabel = false,
+		...otherProps
+	} = props;
+
+	return (
+		<div className={classNames(
+			styles.appInput,
+			className,
+		)}
+		>
+			{hasLabel && (
+				<AppSkeleton
+					height={24}
+					width={100}
+				/>
+			)}
+			<AppSkeleton
+				height={48}
+				color="primary"
+				{...otherProps}
+			/>
+		</div>
+	);
+};
